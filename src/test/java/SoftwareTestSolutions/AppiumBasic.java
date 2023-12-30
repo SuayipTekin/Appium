da7f8c0184ce262e458036972ac6a5463b82b893
Suayip Tekin <suayip.tekin@gmail.com>
2023-12-30 15:56:41 -0600

    created the BaseTest class organized the classes

--- src/test/java/SoftwareTestSolutions/AppiumBasic.java
+++ src/test/java/SoftwareTestSolutions/AppiumBasic.java
@@ -1,5 +1,7 @@
 package SoftwareTestSolutions;
 
+import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import java.io.File;
@@ -12,33 +14,15 @@ import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
 
-public class AppiumBasic {
-	
-	@Test
-	public void AppiumTest() throws MalformedURLException {
-		
-		//AdroidDriver, IOSDriver
-		
-		// Starting the Appium Server
-		AppiumDriverLocalService service = new AppiumServiceBuilder().withAppiumJS(new File("//usr//local//lib//node_modules//appium//build//lib//main.js"))
-		.withIPAddress("127.0.0.1").usingPort(4723).build();
-		service.start();
-		
-		//Appium Code -> Appium Server -> Mobile
-		UiAutomator2Options options = new UiAutomator2Options();
-		options.setDeviceName("TestAutomation");
-		options.setApp("//Users//suayiptekin//eclipse-workspace//Appium//src//test//java//resources//ApiDemos-debug.apk");
-		
-		AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
+public class AppiumBasic extends BaseTest {
 
+    @Test
+    public void WifiSettingsName() throws MalformedURLException {
 
-		
-		// Stopping the server
-//		driver.quit();
-//		service.stop();
-		//Actutal automation starts after the Appium setup
-		
-		
-	}
+        // Locators android locators support appium
+        //1. Xpath, id, Accessibility id, classname, androidUIAutomator
+        driver.findElement(AppiumBy.accessibilityId("Preference")).click();
+
+    }
 
 }
---
+++ src/test/java/SoftwareTestSolutions/BaseTest.java
@@ -0,0 +1,43 @@
+package SoftwareTestSolutions;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+
+import java.io.File;
+import java.net.URL;
+import java.net.MalformedURLException;
+
+
+public class BaseTest {
+
+    public AndroidDriver driver;
+    public AppiumDriverLocalService service;
+
+    @BeforeClass
+    public void ConfigureAppium() throws MalformedURLException {
+
+        //AdroidDriver, IOSDriver
+        // Starting the Appium Server
+        service = new AppiumServiceBuilder().withAppiumJS(new File("//usr//local//lib//node_modules//appium//build//lib//main.js")).withIPAddress("127.0.0.1").usingPort(4723).build();
+        service.start();
+
+        //Appium Code -> Appium Server -> Mobile
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setDeviceName("TestAutomation");
+        options.setApp("//Users//suayiptekin//eclipse-workspace//Appium//src//test//java//resources//ApiDemos-debug.apk");
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
+    }
+
+    @AfterClass
+    public void tearDown() {
+        // Stopping the server
+        driver.quit();
+        service.stop();
+    }
+}
